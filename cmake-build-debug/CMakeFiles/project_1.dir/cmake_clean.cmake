80baa90c8f7abd616cea8bf0b3c171ef07800356
jharwood <jackson.harwood21@gmail.com>
2021-09-11 14:23:48 -0600

    Add CMake files

---
+++ cmake-build-debug/CMakeFiles/project_1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_1.dir/ColonAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/ColonDashAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/Lexer.cpp.obj"
+  "CMakeFiles/project_1.dir/Token.cpp.obj"
+  "CMakeFiles/project_1.dir/main.cpp.obj"
+  "libproject_1.dll.a"
+  "project_1.exe"
+  "project_1.exe.manifest"
+  "project_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
