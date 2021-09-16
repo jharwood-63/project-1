5de6b300c0558899da39096793f960ecf14c1b4c
jharwood <jackson.harwood21@gmail.com>
2021-09-16 11:02:52 -0600

    rm delete

--- Lexer.cpp
+++ Lexer.cpp
@@ -27,9 +27,9 @@ Lexer::Lexer() {
 
 Lexer::~Lexer() {
     // TODO: need to clean up the memory in `automata` and `tokens`
-    for (unsigned int i = 0; i < automata.size(); i++) {
+    /*for (unsigned int i = 0; i < automata.size(); i++) {
         delete automata.at(i);
-    }
+    }*/
     for (unsigned int i = 0; i < tokens.size(); i++) {
         delete tokens.at(i);
     }
